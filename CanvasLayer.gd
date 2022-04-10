e1b1786d9b9287cfa3300a48c117b754920b9fb1
MapReducer <roland_rulez@hotmail.com>
2022-04-10 17:21:15 +0200

    Create more game GUI

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -6,13 +6,14 @@ var food
 var rootscene
 
 func _ready():
-	food = $ui/VBoxContainer/FoodLabel
-	live  = $ui/VBoxContainer/LiveLabel
+	food = $ui/VBoxContainer/HBoxContainer2/FoodLabel
+	live  = $ui/VBoxContainer/HBoxContainer/LiveLabel
 	rootscene = get_tree().root.get_child(0).get_child(2).get_child(0)
+	print(live.name)
 	set_process(true)
 #
 func _process(delta):
 	if rootscene.simulationdone and is_instance_valid(rootscene.activeplayer):
-		food.text = "Food: " + String(rootscene.activeplayer.food)
-		live.text = "Live: " + String(rootscene.activeplayer.live)
+		food.text = String(rootscene.activeplayer.food) + " x "
+		live.text = String(rootscene.activeplayer.live) + " x "
 
